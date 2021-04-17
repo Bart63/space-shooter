c61eb46c9942c06c5c54efc5e7ff5a4bb896bebb
Bart63 <bartek.durys@gmail.com>
2021-04-17 23:03:24 +0200

    Forgot to save one file

--- invaders/Enemy.py
+++ invaders/Enemy.py
@@ -1,13 +1,8 @@
 import Invaders
 
 class Enemy(Invaders):
-    def __init__(self, x, y, movement_speed, collision_damage, health=100):
-        self.x = x
-        self.y = y
-        self.health = health
-        self.movement_speed = movement_speed
-        self.collision_damage = collision_damage
-
+    def __init__(self, x, y, movement_speed, collision_damage, image, health=100):
+        super(Enemy, self).__init__(x, y, movement_speed, collision_damage, image, health)
 
     def move(self):
         pass
