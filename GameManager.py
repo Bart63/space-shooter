66d6bd48d62b6c9b3437c5d7042c444f99afe216
Bart63 <bartek.durys@gmail.com>
2021-04-20 05:32:29 +0200

    Ship movement

--- GameManager.py
+++ GameManager.py
@@ -16,26 +16,42 @@ class GameManager:
     ENEMY_IMG_1 = os.path.join('imgs', 'enemy_new_1.png')
     ENEMY_IMG_2 = os.path.join('imgs', 'enemy_new_2.png')
 
+    def __init__(self):
+        self.SHIP = Ship(200, 200, self.SHIP_IMG_PATH)
+        self.run = True
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.run = False
 
-    def draw(self, SHIP):
+    def draw(self):
         self.WIN.blit(self.BG_IMG, (0, 0))
-        self.WIN.blit(SHIP.img, (SHIP.x, SHIP.y))
+        self.WIN.blit(self.SHIP.img, (self.SHIP.x, self.SHIP.y))
         pygame.display.update()
 
+    def key_manager(self):
+        keys_pressed = pygame.key.get_pressed()
+
+        right, down = 0, 0
+        if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
+            right+=1
+        if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
+            right-=1
+        if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]:
+            down+=1
+        if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
+            down-=1
+        self.SHIP.move(right, down)
+
     def main(self):
-        SHIP = Ship(200, 200, self.SHIP_IMG_PATH)
-        self.run = True
         clock = pygame.time.Clock()
         while self.run:
             clock.tick(self.FPS)
             self.check_events()
-            self.draw(SHIP)
+            self.key_manager()
+            self.draw()
         pygame.quit()
 
 if __name__ == "__main__":
-    gm = GameManager()
-    gm.main()
+    GameManager().main()
