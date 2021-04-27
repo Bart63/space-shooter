7ae580a0eace860dc6880ff1d5469e8c74e52af7
Bart63 <bartek.durys@gmail.com>
2021-04-27 17:25:21 +0200

    PowerUps Base

--- powerups/PowerUps.py
+++ powerups/PowerUps.py
@@ -1,3 +1,19 @@
 class PowerUps:
-    def action(self):
-        pass
+    def __init__(self, is_active, duration):
+        self.is_active = is_active
+        self.duration = duration
+
+    def check_time(self, delta_time):
+        if self.is_active:
+            self.duration -= delta_time
+            if self.duration <= 0:
+                self.desactivation()
+        elif self.duration>0:
+            self.is_active = True
+            self.activation()
+
+    def activation(self):
+        self.is_active = True
+
+    def desactivation(self):
+        self.is_active = False
